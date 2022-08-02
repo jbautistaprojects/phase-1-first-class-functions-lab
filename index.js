f2a58da1caa07d6a1e6df02227079bf2a5e2b38f
Justina Mandaro <bautistajustina7@gmail.com>
2022-08-02 16:02:21 -0700

    Updated code to pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers= function(drivers) {
+    return drivers.slice(0,2);
+  }
+  returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+  
+  const returnLastTwoDrivers= function(drivers) {
+    return drivers.slice(drivers.length-2);
+  }
+ returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+  
+ const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+ 
+
+ function createFareMultiplier(multiplier) {
+    return function(x) {
+    return x * multiplier
+    } 
+    }
+    
+    const fareDoubler = createFareMultiplier(2);
+    
+  fareDoubler(10);
+ 
+  const fareTripler = createFareMultiplier(3);
+
+  fareTripler(12);
+
+  function selectDifferentDrivers(drivers, pickDrivers) {
+    if (pickDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    }
+    else if (pickDrivers === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    }
+  }
+
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers);
+ selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers);
